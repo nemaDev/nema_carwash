7a9871b41f28bdf7238e0c57c849b534de5e3f93
NemaDev <petermannfelipe@gmail.com>
2023-03-31 19:20:07 -0300

    Fix: (Server/not removeitem

--- server/sv_carwash.lua
+++ server/sv_carwash.lua
@@ -1,7 +1,6 @@
-
 RegisterNetEvent('nema_carwash:PayWash', function()
     if Wash.Ticket then
-        exports.ox_inventory:RemoveItem(source, 'wash_ticket', Wash.Amount)
+        exports.ox_inventory:RemoveItem(source, 'wash_ticket', Wash.TicketAmount)
     else
         exports.ox_inventory:RemoveItem(source, 'money', Wash.PriceAmount)
     end
